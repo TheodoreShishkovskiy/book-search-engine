3353c2d414cb26c1a0eae270512b43e5276898af
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-24 21:00:40 -0400

    finished adding PORT route in server.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,16 @@ const path = require('path');
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require('./config/connection');
-const app = express();
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -16,8 +24,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
+  console.log(`Use GraphQL Server here at http://localhost:${PORT}${server.graphqlPath}`);
 });
