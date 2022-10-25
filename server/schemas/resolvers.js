3bef60ebfb10f5a37a422f6d7cb2ca99eec22163
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-24 20:24:34 -0400

    added mutation to save books in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -36,7 +36,18 @@ const resolvers = {
     const token = signToken(user);
     return { token, user };
   },
+  // Mutation to save a book
+  saveBook: async (parent, {newBook}, context) => {
+    if (context.user) {
+      const bookUser = await User.findByIdAndUpdate(
+        {_id: context.user._id}, {$push: {savedBooks: newBook}}, {new: true}
+      );
+      return bookUser;
+    }
+    throw new AuthenticationError('Must be logged in to do this');
+  },
+  
 
 
-
+}
 }
