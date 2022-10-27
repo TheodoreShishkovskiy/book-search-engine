f040295594267f578f415fabc60d47b6105cb924
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-27 15:25:17 -0400

    attempting to fix gql errors

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -39,24 +39,24 @@ const resolvers = {
   // Mutation to save a book
   saveBook: async (parent, {newBook}, context) => {
     if (context.user) {
-      const bookUser = await User.findByIdAndUpdate(
+      const updatedUser = await User.findByIdAndUpdate(
         {_id: context.user._id}, {$push: {savedBooks: newBook}}, {new: true}
       );
-      return bookUser;
+      return updatedUser;
     }
     throw new AuthenticationError('Must be logged in to do this!');
   },
   // Mutation to remove a saved book
   removeBook: async (parent, {bookId}, context) => {
     if (context.user) {
-      const bookUser = await User.findByIdAndUpdate(
+      const updatedUser = await User.findByIdAndUpdate(
         {_id: context.user._id}, {$pull: {savedBooks: {bookId}}}, {new: true}
       );
-      return bookUser;
+      return updatedUser;
     }
     throw new AuthenticationError('Must be logged in to do this!')
   }
 }
-},
+};
 
 module.exports = resolvers;
