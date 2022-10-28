311fc5eec7d74bcd542832ee79dcafa1ca8044c9
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-28 11:35:34 -0400

    fixing token issues in auth.js

--- server/server.js
+++ server/server.js
@@ -21,16 +21,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-
-  console.log(`Use GraphQL Server here at http://localhost:${PORT}${server.graphqlPath}`);
-});
+})
